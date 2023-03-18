a8a720345855a4a3a548ecd11a5507ef27eb6837
tonantr <tonantruong@hotmail.com>
2023-03-17 20:22:06 -0400

    complete Functions_Continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(param = "roller-skate") {
+    return `This Saturday, I want to ${param}!`;
+};
+
+function mondayWork(param = "go to the office") {
+    return `This Monday, I will ${param}.`;
+};
+
+function wrapAdjective(param1 = "*"){
+    return function(param2 = "special") {
+        return `You are ${param1}${param2}${param1}!`;
+    };
+};
